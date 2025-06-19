f9d9d4a8bf7a9f49ce10c799cd36c5d437bed410
Dharmie Folorunso <dharmelorlahfolorunso@gmail.com>
2025-06-19 18:23:21 +0100

    Added JavaScript file

---
+++ quiz.js
@@ -0,0 +1,18 @@
+function checkAnswer() {
+    const correctAnswer = "4";
+    const userAnswer = document.querySelector('input[name="quiz"]:checked').value;
+
+    if (userAnswer === correctAnswer) {
+        const feedback = document.getElementById("feedback")
+        feedback.textContent = "Correct! Well done.";
+    };
+    if (userAnswer !== correctAnswer) {
+        const feedback = document.getElementById("feedback")
+        feedback.textContent = "That's incorrect. Try again!";
+    };
+
+
+};
+
+const submitButton = document.getElementById("submit-answer");
+submitButton.addEventListener("click", checkAnswer);
